b959e7039e4b170892754067227d6f48fe156bcd
novlan1 <1576271227@qq.com>
2022-06-14 10:59:56 +0800

    feat: 增加阿拉伯数字和中文数字转换

--- src/base/number/number.ts
+++ src/base/number/number.ts
@@ -23,3 +23,26 @@ export function getUnitPreviousRatio(value, preValue) {
   }
   return `${symbol}${intervalRatio}%`
 }
+
+export const NUMBER_CHI_MAP = {
+  1: '一',
+  2: '二',
+  3: '三',
+  4: '四',
+  5: '五',
+  6: '六',
+  7: '七',
+  8: '八',
+  9: '九',
+  10: '十',
+  11: '十一',
+  12: '十二',
+  13: '十三',
+  14: '十四',
+  15: '十五',
+  16: '十六',
+  17: '十七',
+  18: '十八',
+  19: '十九',
+  20: '二十',
+}
