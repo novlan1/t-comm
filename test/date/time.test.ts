07f873664444bcb394b44642cfbd54e80d1bff0b
novlan1 <1576271227@qq.com>
2022-09-26 16:06:11 +0800

    feat: 删除time测试用例

--- test/date/time.test.ts
+++ test/date/time.test.ts
@@ -1,12 +1,12 @@
-import { timeStampFormat, getCountDownObj } from '../../src'
+import { getCountDownObj } from '../../src'
 
-describe('timeStampFormat', () => {
-  it('', () => {
-    expect(timeStampFormat(1647503559488, 'yyyy-MM-dd hh:mm:ss')).toBe(
-      '2022-03-17 15:52:39',
-    )
-  })
-})
+// describe('timeStampFormat', () => {
+//   it('', () => {
+//     expect(timeStampFormat(1647503559488, 'yyyy-MM-dd hh:mm:ss')).toBe(
+//       '2022-03-17 15:52:39',
+//     )
+//   })
+// })
 
 describe('getCountDownObj', () => {
   it('', () => {
