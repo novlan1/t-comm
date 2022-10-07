5a5d41f22624617665165aa74adb2a10355bb18d
novlan1 <1576271227@qq.com>
2022-10-07 14:24:56 +0800

    test: loader单测

---
+++ test/loader/loader.test.ts
@@ -0,0 +1,16 @@
+import { loadCSS } from '../../src';
+
+describe('loadCSS', () => {
+  it('loadCSS', () => {
+    const mockFunc = jest.fn();
+    const oriFunc = document.createElement;
+    document.createElement = function (...args) {
+      mockFunc();
+      return oriFunc.apply(this, args);
+    };
+    loadCSS('https://test.css');
+
+    expect(mockFunc).toHaveBeenCalled();
+  });
+});
+
