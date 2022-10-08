58ab0c65f0ed7800e6b16d8861a5d2e2076eb196
novlan1 <1576271227@qq.com>
2022-10-08 12:06:57 +0000

    Deploying to gh-pages from  @ 525180f531f30c56e967097d1060c36511c42e8d 🚀

--- jsdoc.js
+++ jsdoc.js
@@ -1,27 +1,4 @@
-function getSourceFolder(source) {
-  const list = source.split('/');
-  return list.slice(0, list.length - 1).join('/');
-}
 
-function getSeparatorStr(content) {
-  const maxLen = 14;
-  const lastLen = parseInt(maxLen - content.length / 2, 10);
-  const fill = Array.from({ length: lastLen })
-    .map(() => '-')
-    .join('');
-
-  return `${fill}   ${content}   ${fill}`;
-}
-
-function insertSeparator(index, source) {
-  const parent = document.querySelector('nav > ul:nth-of-type(2)');
-  const dom = document.querySelector(`nav > ul:nth-of-type(2) li:nth-child(${index + 1})`);
-  const ele = document.createElement('span');
-  ele.innerHTML = getSeparatorStr(source);
-  ele.style = 'color: hsl(207, 1%, 60%);font-size: 12px;display: block;';
-  ele.classList.add('nav-separator');
-  parent.insertBefore(ele, dom);
-}
 
 function hiddenSeparatorWhenSearch() {
   document.querySelector('#nav-search').addEventListener('input', (event) => {
@@ -46,71 +23,7 @@ function hiddenFooter() {
   }
 }
 
-function getSourceMap() {
-  const LOCAL_STORAGE_KEY = 'T_COMM_SOURCE_MAP';
-  if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
-    return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-  }
-  if (location.href.indexOf('global.html') < 0) return;
-
-  const sourceMap = {};
-  const nameSelector = '#main section article h4.name';
-  const names = document.querySelectorAll(nameSelector);
-
-  for (const item of names) {
-    const detail = item.nextElementSibling;
-    const source = detail.querySelector('dd ul li a').innerHTML.trim();
-
-    sourceMap[item.id] = getSourceFolder(source);
-  }
-  if (Object.keys(sourceMap).length) {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(sourceMap));
-  }
-  return sourceMap;
-}
-
-function getNavList() {
-  const navSelector = 'nav > ul:nth-of-type(2) li a';
-
-  const navs = document.querySelectorAll(navSelector);
-
-  const navList = [];
-
-  for (const nav of navs) {
-    const name = nav.innerHTML.trim();
-    navList.push(name);
-  }
-  return navList;
-}
-
-function insertAllSeparator() {
-  const sourceMap = getSourceMap();
-  const navList = getNavList();
-
-  // console.log('navList: ', navList)
-  // console.log('sourceMap: ', sourceMap)
-
-  if (!Object.keys(sourceMap).length) {
-    return;
-  }
-
-  for (let i = navList.length - 1; i >= 1; i--) {
-    const cur = navList[i];
-    const source = sourceMap[cur];
-    if (!source) {
-      continue
-    }
-    const next = navList[i - 1];
-    const nextSource = sourceMap[next];
-    if (source !== nextSource) {
-      insertSeparator(i, source);
-    }
-  }
-  insertSeparator(0, sourceMap[navList[0]]);
-}
 
 document.addEventListener('DOMContentLoaded', () => {
-  insertAllSeparator();
-  hiddenFooter();
   hiddenSeparatorWhenSearch();
 });
