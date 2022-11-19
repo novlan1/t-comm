da5a89ca959dcc2ad96186a692ae2e5579d48072
novlan1 <1576271227@qq.com>
2022-11-19 10:32:05 +0800

    feat: 增加version-tip的实验

---
+++ script/trial/version-tip.js
@@ -0,0 +1,22 @@
+/**
+ * 测试version-tip功能
+ * 可以修改package.json中的version，来查看不同效果
+ */
+const { genVersionTip, batchSendWxRobotMarkdown } = require('../../lib/index');
+
+const pkg = require('../../package.json');
+
+function main() {
+  const content = genVersionTip({
+    appInfo: pkg,
+    showNpmLink: true,
+    readmeFilePath: './CHANGELOG.md',
+  });
+  batchSendWxRobotMarkdown({
+    content,
+    chatId: 'ALL',
+    webhookUrl: 'a07e58be-696b-482a-a3c7-e2e0ff41c56c', // t-comm更新robot
+  });
+}
+
+main();
