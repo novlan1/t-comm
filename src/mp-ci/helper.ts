010c27220a5f1aca4adcd2028427d1a5a8fa7db9
novlan1 <1576271227@qq.com>
2023-02-14 14:34:57 +0800

    feat: 优化mp-ci

--- src/mp-ci/helper.ts
+++ src/mp-ci/helper.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import { getGitCommitInfo } from '../git/git';
+import { BUNDLE_NAME_MAP } from './config';
+import { formatBite } from '../util/format-bite';
+
 
 export function getBundleBuildDesc({
   root,
@@ -15,4 +18,24 @@ export function getBundleVersion(root) {
   const pkgFile = path.resolve(root, './package.json');
   const pkg = require(pkgFile) || {};
   return pkg.version;
+}
+
+/**
+ * 解析上传结果
+ */
+export function parseUploadResult(result) {
+  const {
+    subPackageInfo,
+  } = result;
+  subPackageInfo.reverse();
+  const list = subPackageInfo.sort((a, b) => {
+    const keys = Object.keys(BUNDLE_NAME_MAP);
+    if (keys.indexOf(a) > -1 || keys.indexOf(b) > -1) {
+      return keys.indexOf(b) - keys.indexOf(a);
+    }
+    return b.size - a.size;
+  })
+    .map(pkg => `- ${BUNDLE_NAME_MAP[pkg.name] || pkg.name}: ${formatBite(pkg.size)}`);
+  list.unshift('PACKAGE SIZE INFO: ');
+  return list;
 }
--- src/mp-ci/index.ts
+++ src/mp-ci/index.ts
@@ -1,2 +1,8 @@
+import * as mpCINew from './new';
 export { MpCI } from './mp-ci';
 export { getBundleBuildDesc, getBundleVersion } from './helper';
+
+export const mpCI = {
+  ...mpCINew,
+};
+
