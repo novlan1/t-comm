dd9d1faecc4adaa3c76cea2152d5bb181ffcc9c0
novlan1 <1576271227@qq.com>
2023-03-22 12:49:47 +0800

    chore: 优化cdn测试脚本

--- script/trial/cdn/cdn.js
+++ script/trial/cdn/cdn.js
@@ -4,8 +4,13 @@ require('../../utils/env');
 
 function main() {
   const {
-    OVERSEAS_CLOUD_SECRET_ID: secretId,
-    OVERSEAS_CLOUD_SECRET_KEY: secretKey,
+    // 海外用这个
+    // OVERSEAS_CLOUD_SECRET_ID: secretId,
+    // OVERSEAS_CLOUD_SECRET_KEY: secretKey,
+
+    // 国内用这个
+    CLOUD_API_SECRET_ID: secretId,
+    CLOUD_API_SECRET_KEY: secretKey,
   } = process.env;
   console.log('secretKey', secretKey);
   console.log('secretId', secretId);
@@ -14,8 +19,10 @@ function main() {
     secretId,
     secretKey,
     urls: [
-      'https://cyberimage.sgameglobal.com/static/js/chunk-core-js.d2f9f03b.js',
-      'https://cyberimage.sgameglobal.com/static/js/runtime.ec954923.js',
+      // 'https://cyberimage.sgameglobal.com/static/js/chunk-core-js.d2f9f03b.js',
+      // 'https://cyberimage.sgameglobal.com/static/js/runtime.ec954923.js',
+
+      'https://image-1251917893.file.myqcloud.com/static/js/chunk-vendors.d789d61f.js',
     ],
   }).then((res) => {
     console.log('[main] res: ', res.data);
