214a1c472757e7a729aaefd30cb17925317e83a6
novlan1 <1576271227@qq.com>
2023-06-15 10:59:26 +0800

    chore: 增加init env

---
+++ script/env/rainbow-env.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const { writeEnvFromRainbow } = require('../../lib');
+
+const envPath = path.resolve(__dirname, '../../.env.local');
+
+async function main() {
+  await writeEnvFromRainbow({
+    envPath,
+    rainbowKey: 't_comm_env',
+    envName: 'Default',
+    groupName: 'devops',
+  });
+}
+
+main();
