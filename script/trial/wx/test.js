eda221f2739cfba8300c4978d0878ee47f8366bd
novlan1 <1576271227@qq.com>
2023-07-20 18:17:00 +0800

    chore: eslint

--- script/trial/wx/test.js
+++ script/trial/wx/test.js
@@ -1,14 +1,14 @@
 require('../../utils/env');
 const {
-  getAccessToken,
+  // getAccessToken,
   getAPITicket,
-  genSignature,
+  // genSignature,
 } = require('../../../lib');
 
-const appId = process.env.WX_MP_APP_ID;
-const appSecret = process.env.WX_MP_APP_SECRET;
+// const appId = process.env.WX_MP_APP_ID;
+// const appSecret = process.env.WX_MP_APP_SECRET;
 const accessToken = '70_bOnWzTiCoQhWG_GX8xMJysSsCwow54bZJKpRGCca01DIV-9d4etsYzOZGjaejJvlMbLM6BfN3kioaz-yacLZ9GdIs7WMNOx81TxPq6lYhD2maB6l8wVdY9oNKIkFWFaAFAZQZ';
-const ticket = 'sM4AOVdWfPE4DxkXGEs8VPvn47x7T8NnhIdMaLFatYX7z5OlNqOPxhMUgxsZoVgCTZ3V9Avg6hK3Lt-BruBjsw';
+// const ticket = 'sM4AOVdWfPE4DxkXGEs8VPvn47x7T8NnhIdMaLFatYX7z5OlNqOPxhMUgxsZoVgCTZ3V9Avg6hK3Lt-BruBjsw';
 
 
 function main() {
