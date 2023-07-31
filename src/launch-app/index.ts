93ee8736d4e55644ff246fd7e5afccb6285a369c
novlan1 <1576271227@qq.com>
2023-07-31 09:53:09 +0800

    feat(launch-app): 移除

--- src/launch-app/index.ts
+++ src/launch-app/index.ts
@@ -1,14 +1,1 @@
 export { launchApp } from './core';
-export {
-  gotoDzGame,
-  gotoGPGame,
-  gotoLOLMGame,
-  gotoMJGame,
-  gotoTDGame,
-  gotoTLBBGame,
-  gotoWzCommunity,
-  gotoWzGame,
-  gotoX5MGame,
-  gotoNarutoGame,
-  getOpenGameScheme,
-} from './launch-app';
