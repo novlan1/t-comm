47c8d6f7c583dcf4da953797fe55bbc1047829d1
novlan1 <1576271227@qq.com>
2023-08-01 12:05:43 +0800

    refactor(loader): 移除多余的注释

--- src/loader/little-loader.ts
+++ src/loader/little-loader.ts
@@ -1,34 +1,7 @@
-/**
- * @module tools/little-loader
- * @description  用户程序化在页面加载js文件，修改https://github.com/walmartlabs/little-loader，支持指定编码，支持排重
- * 这个文件比较好的地方在于支持IE<10的js加载后回调，IE<10占比再小一点的话，可以去掉。
- */
-
-/**
- * Script loading is difficult thanks to IE. We need callbacks to fire
- * immediately following the script's execution, with no other scripts
- * running in between. If other scripts on the page are able to run
- * between our script and its callback, bad things can happen, such as
- * `jQuery.noConflict` not being called in time, resulting in plugins
- * latching onto our version of jQuery, etc.
- *
- * For IE<10 we use a relatively well-documented "preloading" strategy,
- * which ensures that the script is ready to execute *before* appending
- * it to the DOM. That way when it is finally appended, it is
- * executed immediately.
- *
- * References:
- * - http://www.html5rocks.com/en/tutorials/speed/script-loading/
- * - http://blog.getify.com/ie11-please-bring-real-script-preloading-back/
- * - https://github.com/jrburke/requirejs/issues/526
- * - https://connect.microsoft.com/IE/feedback/details/729164/
- *           ie10-dynamic-script-element-fires-loaded-readystate-prematurely
- */
-// Global state.
 const pendingScripts: Record<string, any> = {};
 let scriptCounter = 0;
 
-const _addScript = function (script: any) {
+const addScript = function (script: any) {
   // Get the first script element, we're just going to use it
   // as a reference for where to insert ours. Do NOT try to do
   // this just once at the top and then re-use the same script
@@ -48,9 +21,7 @@ const _addScript = function (script: any) {
    * @param {String}          charset  指定js的字符集
    * @param {Object}          context Callback context
    */
-// eslint-disable-next-line max-statements
 export const loader = function (src: string, callback: any, charset = 'utf-8', context = null) {
-  /* eslint max-statements: [2, 32]*/
   let setup;
 
   if (callback && typeof callback !== 'function') {
@@ -142,39 +113,15 @@ export const loader = function (src: string, callback: any, charset = 'utf-8', c
           inserted = true;
 
           // Append to DOM.
-          _addScript(script);
+          addScript(script);
         }
 
-        // --------------------------------------------------------------------
-        //                       GLORIOUS IE8 HACKAGE!!!
-        // --------------------------------------------------------------------
-        //
-        // Oh IE8, how you disappoint. IE8 won't call `script.onerror`, so
-        // we have to resort to drastic measures.
-        // See, e.g. http://www.quirksmode.org/dom/events/error.html#t02
-        //
-        // As with all things development, there's a Stack Overflow comment that
-        // asserts the following combinations of state changes in IE8 indicate a
-        // script load error. And crazily, it seems to work!
-        //
-        // http://stackoverflow.com/a/18840568/741892
-        //
-        // The `script.readyState` transitions we're interested are:
-        //
-        // * If state starts as `loaded`
-        // * Call `script.children`, which _should_ change state to `complete`
-        // * If state is now `loading`, then **we have a load error**
-        //
-        // For the reader's amusement, here is HeadJS's catalog of various
-        // `readyState` transitions in normal operation for IE:
-        // https://github.com/headjs/headjs/blob/master/src/2.0.0/load.js#L379-L419
+
         if (firstState === 'loaded') {
           // The act of accessing the property should change the script's
           // `readyState`.
           //
           // And, oh yeah, this hack is so hacky-ish we need the following
-          // eslint disable...
-          /* eslint-disable no-unused-expressions*/
           // eslint-disable-next-line @typescript-eslint/no-unused-expressions
           script.children;
 
@@ -244,7 +191,7 @@ export const loader = function (src: string, callback: any, charset = 'utf-8', c
       script.src = src;
 
       // Append to DOM.
-      _addScript(script);
+      addScript(script);
     }
   }
 };
