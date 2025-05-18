44463d0c8413e90268662f2be46ff9ad2813bbf6
novlan1 <1576271227@qq.com>
2025-05-18 23:32:21 +0800

    docs: update docs

--- script/docs-jsdoc/deploy-docs.js
+++ script/docs-jsdoc/deploy-docs.js
@@ -4,7 +4,7 @@ const ENV_FILE = '.env.local';
 const CLI_FILE = './bin/cli';
 const DEPLOY_CONFIG = {
   repo: 'docs',
-  targetDir: './dist/build/h5',
+  targetDir: './docs/.vuepress/dist/',
   branch: 'main',
 
   commitMessage: 'docs: update docs of t-comm',
@@ -17,10 +17,11 @@ function main() {
   require('dotenv').config({ path: ENV_FILE });
 
   execSync(`node ${CLI_FILE} deploy:github --repo ${DEPLOY_CONFIG.repo} \
-    --dir ${DEPLOY_CONFIG.targetDir}
-    --branch ${DEPLOY_CONFIG.branch}
-    --commitMessage ${DEPLOY_CONFIG.commitMessage}
-    --playgroundDir ${DEPLOY_CONFIG.playgroundDir}
+    --dir ${DEPLOY_CONFIG.targetDir} \
+    --branch ${DEPLOY_CONFIG.branch} \
+    --increment 1 \
+    --message ${DEPLOY_CONFIG.commitMessage} \
+    --playgroundDir ${DEPLOY_CONFIG.playgroundDir} \
     --targetDirName ${DEPLOY_CONFIG.targetDirName}
     `, {
     stdio: 'inherit',
